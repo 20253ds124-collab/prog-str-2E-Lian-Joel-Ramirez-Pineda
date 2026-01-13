1bca27106a107a428a460b73e13ddee4614744ae
Lian Ramirez <20253ds124@utez.edu.mx>
2026-01-13 07:27:11 -0600

    se edita el archivo readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # prog-str-2E-Lian-Joel-Ramirez-Pineda
+Este es un repo para entregar los trabajos de la materia de programacion 
--- proyecto-01-hola-mundo/src/Main.java
+++ proyecto-01-hola-mundo/src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main (String[]args) {
+        System.out.println("Hola mundo");
+        System.out.println("Lian joel ramirez pineda");
+        System.out.println("2 E");
+        System.out.println("9/1/26");
+    }
 }
